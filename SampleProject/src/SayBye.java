639e5495c71de64594e0c5fd7bd18af4e64e0248
Taher Moosa <taher@entelect.co.za>
2014-08-08 09:26:40 +0200

    second file

---
+++ SampleProject/src/SayBye.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: taher
+ * Date: 2014/08/08
+ * Time: 9:06 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SayBye {
+
+
+    public static void main(String [] args){
+
+        System.out.println("Bye Bye");
+    }
+
+}
