b3eeed6ed3e7c4f562e972711afaa96530820e62
Taher Moosa <taher@entelect.co.za>
2014-08-08 10:22:39 +0200

    did the merge work

--- SampleProject/src/SayBye.java
+++ SampleProject/src/SayBye.java
@@ -10,7 +10,7 @@ public class SayBye {
 
     public static void main(String [] args){
 
-        System.out.println("Bye Bye Taher Moosa Nathie");
+        System.out.println("Bye Bye Someone");
     }
 
 }
